fcb8ddf54fbada26a42024f111e5f78571c828a9
Giu <120753646+Giu-11@users.noreply.github.com>
2025-10-17 13:03:15 -0300

    ✨feat: struct carta

--- pbl/shared/models.go
+++ pbl/shared/models.go
@@ -3,9 +3,9 @@ package shared
 import "encoding/json"
 
 type Event struct {
-    Type string          `json:"type"` 
-    Data json.RawMessage `json:"data"` 
-    From int             `json:"from"`
+	Type string          `json:"type"`
+	Data json.RawMessage `json:"data"`
+	From int             `json:"from"`
 }
 
 type User struct {
@@ -15,17 +15,22 @@ type User struct {
 	Deck     []string `json:"deck"`
 }
 
+type Card struct {
+	Element string `json:"element"`
+	Type    string `json:"type"`
+	Id      int    `json:"id"`
+}
+
 type Request struct {
-    ClientID string `json:"client_id"`
-    Action   string `json:"action"`
-    Payload  json.RawMessage `json:"payload"`
+	ClientID string          `json:"client_id"`
+	Action   string          `json:"action"`
+	Payload  json.RawMessage `json:"payload"`
 }
 
 type Response struct {
-    Status   string          `json:"status"`
-    Action   string          `json:"action"`
-    Data     json.RawMessage `json:"data,omitempty"`
-    Error    string          `json:"error,omitempty"`
-    Server int             `json:"server"`
+	Status string          `json:"status"`
+	Action string          `json:"action"`
+	Data   json.RawMessage `json:"data,omitempty"`
+	Error  string          `json:"error,omitempty"`
+	Server int             `json:"server"`
 }
-
